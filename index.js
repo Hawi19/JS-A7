12e0ed702d10895926da2d560843547457aae712
Hawi19 <168331546+Hawi19@users.noreply.github.com>
2024-10-16 03:58:49 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+//number 1
+const colors =["red", "green", "blue"]
+const[firstColor, secondColor, thirdColor] = colors;
+
+//number 2
+const person = { name: 'Alice', age: 25, city: 'Wonderland' }
+const{name, age, city} = person;
+//number 3
+
+const settings ={ theme: 'dark', language: 'en',};
+   const {theme, language, mode ='light'}= settings
